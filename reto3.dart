d0310a50b86a1a3e945387e70a9974f2292e92ca
Maria Aguilar <maria.aguilar.c@tecsup.edu.pe>
2021-05-27 23:05:46 -0500

    correccion

--- reto3.dart
+++ reto3.dart
@@ -1,106 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/physics.dart';
 
-void main() {
-  runApp(MaterialApp(home: PhysicsCardDragDemo()));
-}
-
-class PhysicsCardDragDemo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: DraggableCard(
-        child: FlutterLogo(
-          size: 128,
-        ),
+void main() => runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Cont(),
       ),
-    );
+    )));
+
+class Cont extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ContEstado();
   }
 }
 
-class DraggableCard extends StatefulWidget {
-  final Widget child;
-  DraggableCard({required this.child});
+class _ContEstado extends State<Cont> {
+  int i = 0;
 
-  @override
-  _DraggableCardState createState() => _DraggableCardState();
-}
-
-class _DraggableCardState extends State<DraggableCard>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  Alignment _dragAlignment = Alignment.center;
-
-  late Animation<Alignment> _animation;
-
-
-  void _runAnimation(Offset pixelsPerSecond, Size size) {
-    _animation = _controller.drive(
-      AlignmentTween(
-        begin: _dragAlignment,
-        end: Alignment.center,
-      ),
-    );
-
-    final unitsPerSecondX = pixelsPerSecond.dx / size.width;
-    final unitsPerSecondY = pixelsPerSecond.dy / size.height;
-    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
-    final unitVelocity = unitsPerSecond.distance;
-
-    const spring = SpringDescription(
-      mass: 30,
-      stiffness: 1,
-      damping: 1,
-    );
-
-    final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
-
-    _controller.animateWith(simulation);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-
-    _controller.addListener(() {
-      setState(() {
-        _dragAlignment = _animation.value;
-      });
+  void incrementar() {
+    setState(() {
+      i++;
     });
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onPanDown: (details) {
-        _controller.stop();
-      },
-      onPanUpdate: (details) {
-        setState(() {
-          _dragAlignment += Alignment(
-            details.delta.dx / (size.width / 2),
-            details.delta.dy / (size.height / 2),
-          );
-        });
-      },
-      onPanEnd: (details) {
-        _runAnimation(details.velocity.pixelsPerSecond, size);
-      },
-      child: Align(
-        alignment: _dragAlignment,
-        child: Card(
-          child: widget.child,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        
+        ElevatedButton(
+          //
+          onPressed: incrementar,
+          child: Text('Pulsar para incrementar'), 
         ),
-      ),
+        SizedBox(width: 20),
+        Text('Countador: $i'),
+      ],
     );
   }
 }
